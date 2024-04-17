d389190b352c4fefa5d15a4d85c29f6bf633bb56
NarekTashchyan <narek.tashchian@gmail.com>
2024-04-17 22:48:33 +0400

    Task

--- service/userService.js
+++ service/userService.js
@@ -1,6 +1,5 @@
 // userService.js
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const { deleteUser } = require("../controllers/userController");
 
